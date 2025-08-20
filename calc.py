5d9370915b1edfb18e4e09fb496378ab940e805b
Abubakar50C <abubakara2005@gmail.com>
2025-08-20 15:59:10 -0400

    added variable for user to choose operation

--- calc.py
+++ calc.py
@@ -12,5 +12,22 @@ def div(a, b):
     if b == 0:
         return 'Error: Cannot divide by 0.'
     return a / b
+def exp(a, b):
+    return a ** b
+
+#Defined calculator function, created loop.
+def calculator():
+
+    print('Welcome to the CLI Python Calculator')
+
+    while True:
+        print('Choose an operation')
+        print('1) Addition')
+        print('2) Subtraction')
+        print('3) Multiplication')
+        print('4) Division')
+        print('5) Exponent ^')
+
+        choice = input('Enter operation number: ')
 
     
