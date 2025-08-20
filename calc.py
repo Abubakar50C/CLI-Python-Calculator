0aaab20e04771d330d724a5763e0d34c2c28b412
Abubakar50C <abubakara2005@gmail.com>
2025-08-20 16:06:35 -0400

    added error handling logic

--- calc.py
+++ calc.py
@@ -21,13 +21,29 @@ def calculator():
     print('Welcome to the CLI Python Calculator')
 
     while True:
-        print('Choose an operation')
+        print ('Choose an operation')
         print('1) Addition')
         print('2) Subtraction')
         print('3) Multiplication')
         print('4) Division')
         print('5) Exponent ^')
+        print('6) Exit')
 
         choice = input('Enter operation number: ')
+
+        if choice == '6':
+            print('Goodbye, thanks for using the CLI Python Calculator!')
+            break
+        if choice not in ["1", "2", "3", "4", "5"]:
+            print("Invalid choice. Please select 1-6.")
+            continue
+
+        try:
+            a = float(input("Enter first number: "))
+            b = float(input("Enter second number: "))   
+        except ValueError:
+            print("Invalid input. Please enter numeric values.")
+            continue 
+
 
     
